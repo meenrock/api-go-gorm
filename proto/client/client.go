7d1d55f989366335e7b6deac29f732c5664c43ae
NOVAHOrizonVelocity <77928053+meenrock@users.noreply.github.com>
2023-09-15 22:27:58 +0700

    add

---
+++ proto/client/client.go
@@ -0,0 +1,42 @@
+package proto
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	pb "restapi/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	defaultName = "meen"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
+
+func StartGrpcClient() {
+	flag.Parse()
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewUserServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	//r, err := c.GetUserGrpc(ctx, &pb.UserRequest{})
+	r, err := c.GetUser(ctx, &pb.UserRequest{})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetFirstName())
+
+}
