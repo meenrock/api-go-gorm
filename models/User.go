0f653736175bf54bffcfeba4803283f80d2de299
NOVAHOrizonVelocity <77928053+meenrock@users.noreply.github.com>
2023-09-17 22:19:30 +0700

    update

--- models/User.go
+++ models/User.go
@@ -11,11 +11,11 @@ type User struct {
 	NickName     string `json:"nick_name"`
 	Email        string `json:"email"`
 	PhoneNumber  string `json:"phone_number"`
-	Weight       int
-	Height       int
+	Weight       float32
+	Height       float32
 	Age          int
 	Sex          string `json:"sex"`
 	AllergicFood string `json:"allergic_food"`
 	FavFood      string `json:"fav_food"`
-	ExpectedBMI  int
+	ExpectedBMI  float32
 }
--- proto/server/server.go
+++ proto/server/server.go
@@ -23,6 +23,63 @@ type GrpcServerImpl struct {
 	pb.UnimplementedMealServiceServer
 }
 
+func (s *GrpcServerImpl) GetUser(req *pb.Empty, stream pb.UserService_GetUserServer) error {
+
+	db, err := database.ConnectDBPostgres()
+	if err != nil {
+		return nil
+	}
+	defer db.Close()
+
+	var users []models.User
+	if err := db.Find(&users).Error; err != nil {
+		return nil
+	}
+
+	// userResponses := make([]*pb.UserResponseAll, len(users))
+	// for i, user := range users {
+	// 	userResponses[i] = &pb.UserResponseAll{
+	// 		Id:           int32(user.ID),
+	// 		FirstName:    user.FirstName,
+	// 		LastName:     user.LastName,
+	// 		NickName:     user.NickName,
+	// 		PhoneNumber:  user.PhoneNumber,
+	// 		Email:        user.Email,
+	// 		Sex:          user.Sex,
+	// 		Age:          int32(user.Age),
+	// 		Height:       float32(user.Height),
+	// 		Weight:       float32(user.Weight),
+	// 		AllergicFood: user.AllergicFood,
+	// 		FavFood:      user.FavFood,
+	// 		ExpectedBMI:  float32(user.ExpectedBMI),
+	// 	}
+	// }
+
+	for _, user := range users {
+		userResponses := &pb.UserResponseAll{
+			Id:           int32(user.ID),
+			FirstName:    user.FirstName,
+			LastName:     user.LastName,
+			NickName:     user.NickName,
+			PhoneNumber:  user.PhoneNumber,
+			Email:        user.Email,
+			Sex:          user.Sex,
+			Age:          int32(user.Age),
+			Height:       float32(user.Height),
+			Weight:       float32(user.Weight),
+			AllergicFood: user.AllergicFood,
+			FavFood:      user.FavFood,
+			ExpectedBMI:  float32(user.ExpectedBMI),
+		}
+
+		if err := stream.Send(userResponses); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func (s *GrpcServerImpl) GetUserById(ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
 
 	db, err := database.ConnectDBPostgres()
@@ -46,9 +103,19 @@ func (s *GrpcServerImpl) GetUserById(ctx context.Context, req *pb.UserRequest) (
 	// response := &pb.UserResponse{}
 
 	response := &pb.UserResponse{
-		Id:        int32(user.ID),
-		FirstName: user.FirstName,
-		LastName:  user.LastName,
+		Id:           int32(user.ID),
+		FirstName:    user.FirstName,
+		LastName:     user.LastName,
+		NickName:     user.NickName,
+		PhoneNumber:  user.PhoneNumber,
+		Email:        user.Email,
+		Sex:          user.Sex,
+		Age:          int32(user.Age),
+		Height:       float32(user.Height),
+		Weight:       float32(user.Weight),
+		AllergicFood: user.AllergicFood,
+		FavFood:      user.FavFood,
+		ExpectedBMI:  float32(user.ExpectedBMI),
 	}
 
 	return response, nil
@@ -63,8 +130,18 @@ func (s *GrpcServerImpl) CreateUser(ctx context.Context, req *pb.UserCreateReque
 	defer db.Close()
 
 	user := &models.User{
-		FirstName: req.FirstName,
-		LastName:  req.LastName,
+		FirstName:    req.FirstName,
+		LastName:     req.LastName,
+		NickName:     req.NickName,
+		PhoneNumber:  req.PhoneNumber,
+		Email:        req.Email,
+		Sex:          req.Sex,
+		Age:          int(req.Age),
+		Height:       float32(req.Height),
+		Weight:       float32(req.Weight),
+		AllergicFood: req.AllergicFood,
+		FavFood:      req.FavFood,
+		ExpectedBMI:  float32(req.ExpectedBMI),
 	}
 
 	if err := db.Create(user).Error; err != nil {
@@ -73,17 +150,90 @@ func (s *GrpcServerImpl) CreateUser(ctx context.Context, req *pb.UserCreateReque
 
 	db.Create(&user)
 
-	// userResponses := make([]*pb.UserResponse, len(users))
-	// for i, user := range users {
-	// 	userResponses[i] = &pb.UserResponse{
-	// 		FirstName: user.FirstName,
-	// 		LastName:  user.LastName,
-	// 	}
-	// }
 	response := &pb.UserResponse{
-		Id:        int32(user.ID),
-		FirstName: user.FirstName,
-		LastName:  user.LastName,
+		Id:           int32(user.ID),
+		FirstName:    user.FirstName,
+		LastName:     user.LastName,
+		NickName:     user.NickName,
+		PhoneNumber:  user.PhoneNumber,
+		Email:        user.Email,
+		Sex:          user.Sex,
+		Age:          int32(user.Age),
+		Height:       float32(user.Height),
+		Weight:       float32(user.Weight),
+		AllergicFood: user.AllergicFood,
+		FavFood:      user.FavFood,
+		ExpectedBMI:  float32(user.ExpectedBMI),
+	}
+
+	return response, nil
+}
+
+func (s *GrpcServerImpl) EditUser(ctx context.Context, req *pb.UserEditRequest) (*pb.UserResponse, error) {
+
+	db, err := database.ConnectDBPostgres()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	user := &models.User{
+		FirstName:    req.FirstName,
+		LastName:     req.LastName,
+		NickName:     req.NickName,
+		PhoneNumber:  req.PhoneNumber,
+		Email:        req.Email,
+		Sex:          req.Sex,
+		Age:          int(req.Age),
+		Height:       float32(req.Height),
+		Weight:       float32(req.Weight),
+		AllergicFood: req.AllergicFood,
+		FavFood:      req.FavFood,
+		ExpectedBMI:  float32(req.ExpectedBMI),
+	}
+
+	if err := db.First(user, req.Id).Error; err != nil {
+		return nil, err
+	}
+
+	db.Save(&user)
+
+	response := &pb.UserResponse{
+		Id:           int32(user.ID),
+		FirstName:    user.FirstName,
+		LastName:     user.LastName,
+		NickName:     user.NickName,
+		PhoneNumber:  user.PhoneNumber,
+		Email:        user.Email,
+		Sex:          user.Sex,
+		Age:          int32(user.Age),
+		Height:       float32(user.Height),
+		Weight:       float32(user.Weight),
+		AllergicFood: user.AllergicFood,
+		FavFood:      user.FavFood,
+		ExpectedBMI:  float32(user.ExpectedBMI),
+	}
+
+	return response, nil
+}
+
+func (s *GrpcServerImpl) DeleteUser(ctx context.Context, req *pb.UserRequest) (*pb.UserDeleteConfirmation, error) {
+
+	db, err := database.ConnectDBPostgres()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	var user models.User
+	if err := db.First(&user, req.Id).Error; err != nil {
+		return nil, err
+	}
+
+	db.Delete(&user)
+
+	response := &pb.UserDeleteConfirmation{
+		Id: int32(user.ID),
 	}
 
 	return response, nil
